fd3a56b75fcf017e41d75e39195587df7aa82687
Kaan Yucel <78034265+wtrfl@users.noreply.github.com>
2026-04-27 19:40:43 -0700

    remove unnecessary queue class

--- independent.js
+++ independent.js
@@ -1,33 +1,3 @@
-class Queue {
-    constructor() {
-        this.items = {}
-        this.headIndex = 0
-        this.tailIndex = 0
-    }
-
-    enqueue(item) {
-        this.items[this.tailIndex] = item
-        this.tailIndex++
-    }
-
-    dequeue() {
-        const item = this.items[this.headIndex];
-        delete this.items[this.headIndex];
-        this.headIndex++;
-        return item;
-    }
-
-    size() {
-        return this.tailIndex - this.headIndex;
-    }
-
-    clear() {
-        this.items = {};
-        this.headIndex = 0;
-        this.tailIndex = 0;
-    }
-}
-
 const independent = {
     base: {
         short: 'E',
